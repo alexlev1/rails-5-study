3f614a9d8a192519cc24b0ec282a1ba3da3d2bf4
Alex Levashov <alevashov1@gmail.com>
2017-09-29 15:14:45 +0300

    Add gems which work with a images

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170923211614) do
+ActiveRecord::Schema.define(version: 20170929112501) do
 
   create_table "pages", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20170923211614) do
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
   end
 
 end
